4a482d721635adc857826ce0a0e2d87c7ecae36a
Milan Zinzuvadiya <milanzinzuvadiya99@gmail.com>
2020-07-05 23:42:47 -0400

    Creation of Todo app
    using Node JS , Express and mongodb

---
+++ Day 7 To do app/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+
+//set view engine : ejs
+app.set("view engine","ejs");
+app.use(express.static('public'));
+
+
+
+// express route
+app.get("/",function(req,res){
+    var tasksDB = ["100DaysOfCode","Study Research Paper","Develope Reinforcement Algorithm","Practice LInear Algebra","read Master Algorithm"];
+    res.render(`index`,{tasks:tasksDB});
+});
+
+
+app.get("*",function(req,res){
+    res.send("<h1>Invalid route</h1>");
+});
+
+
+// server listening port
+
+app.listen(3000,function(){
+    console.log("server started on port 3000");
+});
